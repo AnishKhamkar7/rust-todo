726d7beb398df9e2a472b36fe33a822fbda2425e
Anish <anishkhamkar7@gmail.com>
2025-02-24 00:00:07 +0530

    Added basic I/O in cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use  std::io::{stdout,stdin,Write};
+
 fn main() {
-    println!("Hello, world!");
-}
+    
+    let mut  s = String::new();
+    println!("Enter Some text");
+
+    let _ = stdout().flush(); 
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    println!("Yout typed {}",s);
+}  
