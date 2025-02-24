80325150a04e5bb464f5fe2ec0478a4e69515f3e
Anish <anishkhamkar7@gmail.com>
2025-02-24 09:05:55 +0530

    Initialized Functions for todos

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 use  std::io::{stdout,stdin,Write};
 
+static mut todos:Vec<String> = Vec::new(); 
+
+fn addTodos(){
+
+}
+
+fn deleteTodo(){
+
+}
+
+fn doneTodo(){
+    
+}
+
+
+
 fn main() {
     
     let mut  s = String::new();
-    println!("Enter Some text");
-
     let _ = stdout().flush(); 
-    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    stdin().read_line( &mut s).expect("Did not enter a correct string");
     if let Some('\n')=s.chars().next_back() {
         s.pop();
     }
